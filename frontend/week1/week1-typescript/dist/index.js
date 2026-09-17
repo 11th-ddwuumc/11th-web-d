336278aac41df5a3044589778a070e33bede28f0
lojhbm <lojhbm@gmail.com>
2026-09-17 10:38:52 +0900

    feat: 필수 미션 스터디 회원 관리 프로그램 완성하기 완료

---
+++ frontend/week1/week1-typescript/dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+const members = [
+    { memberId: "1", name: "광수", level: 1, isLeader: true, githubId: "gwangsoo" },
+    { memberId: 2, name: "지수", level: 2, isLeader: false },
+];
+function getMemberInfo(memberId) {
+    const member = members.find((member) => String(member.memberId) === String(memberId));
+    if (!member) {
+        return "존재하지 않는 회원입니다.";
+        // console.log("존재하지 않는 회원입니다.");
+    }
+    else if (!member.githubId) {
+        return member.name + "존재하지 않는 회원입니다.";
+        // console.log(member.name+ "존재하지 않는 회원입니다.");
+    }
+    else {
+        return member.name + "님의 GitHub 아이디는" + member.githubId + "입니다.";
+        // console.log(member.name+ "님의 GitHub 아이디는"+member.githubId+"입니다.");
+    }
+}
+console.log(getMemberInfo(1));
+console.log(getMemberInfo(2));
+console.log(getMemberInfo(999));
